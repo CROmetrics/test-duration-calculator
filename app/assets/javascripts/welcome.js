991cfe1cf719c995ffd17d334d3a9b8b18b3be2e
Tom Fuertes <tomfuertes@gmail.com>
2016-03-07 15:16:25 -0500

    run on domReady

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,32 +6,33 @@ $(function() {
     $("#sample-size-calculator input").on("mousedown keyup", function() {
         validate();
     });
+    validate(true);
 });
 
-function validate() {
+function validate(supress) {
     var errors = "";
-    if ($("#existing_conversions").val() == "")
+    if ($("#existing_conversions").val() === "")
         errors += "- Please enter your expected conversion rate.\n";
     else if (isNaN($("#existing_conversions").val()) || $("#existing_conversions").val() > 100 || $("#existing_conversions").val() <= 0)
         errors += "- Please enter a valid number for the conversion rate.\n";
-    if ($("#versions").val() == "")
+    if ($("#versions").val() === "")
         errors += "- Please enter the number of combinations you have.\n";
     else if (isNaN($("#versions").val()) || $("#versions").val() <= 0)
         errors += "- Please enter a valid number for the combinations you have.\n";
-    if ($("#improvement").val() == "")
+    if ($("#improvement").val() === "")
         errors += "- Please enter desired change in conversion rate.\n";
     else if (isNaN($("#improvement").val()) || $("#improvement").val() <= 0)
         errors += "- Please enter a valid value for desired change in conversion rate.\n";
-    if ($("#percent").val() == "")
+    if ($("#percent").val() === "")
         errors += "- Please enter the percentage of visitors to include in the test.\n";
     else if (isNaN($("#percent").val()) || $("#percent").val() <= 0 || $("#percent").val() > 100)
         errors += "- Please enter a valid number for percentage of visitors to include in the test.\n";
-    if ($("#visitors").val() == "")
+    if ($("#visitors").val() === "")
         errors += "- Please enter the average number of visitors on the test page.\n";
     else if (isNaN($("#visitors").val()) || $("#visitors").val() < 0)
         errors += "- Please enter a valid number for the average number of visitors on the test page.\n";
     if (errors.length > 0) {
-        alert(errors);
+        if (!supress) {alert(errors);}
         return false;
     } else {
         var meanSize;
@@ -49,7 +50,7 @@ function validate() {
         var perVariationResult = Math.pow((4) * (stdDev / effectSize), 2);
         var result = perVariationResult * (numberOfVariations);
         result = Math.round(Math.round(result, 0) / perDayVisit);
-        if (result == 0)
+        if (result === 0)
             $("#results").html("Less than a day");
         else
             $("#results").html("<strong>" + result + "</strong> day" + (result > 1 ? "s" : ""));
